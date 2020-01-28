8071315c6bcbb9736f203f934c74ade439f8d800
Andrei Ledauski <c-aledauski@neogov.net>
2020-01-28 15:12:07 +0300

    Add header, teacher and open classes

--- diplom/src/app/app-routing.module.ts
+++ diplom/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeachersComponent } from './teachers/teachers.component';
+import { OpenClassesComponent } from './open-classes/open-classes.component';
+import { ScientificAndMethodologicalSupportComponent } from './scientific-and-methodological-support/scientific-and-methodological-support.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'teachers',
+    component: TeachersComponent
+  },
+  {
+    path: 'openClasses',
+    component: OpenClassesComponent
+  },
+  {
+    path: 'support',
+    component: ScientificAndMethodologicalSupportComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
