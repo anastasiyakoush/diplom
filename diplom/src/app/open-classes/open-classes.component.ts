801486a95d16f8a7087f30ce3365b4d7c8c00f9b
Elizabeth <liza.babior@gmail.com>
2020-01-31 22:10:24 +0300

    " replaced by '

--- diplom/src/app/open-classes/open-classes.component.ts
+++ diplom/src/app/open-classes/open-classes.component.ts
@@ -2,35 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Lesson } from './lesson.model';
 
 @Component({
-  selector: "app-open-classes",
-  templateUrl: "./open-classes.component.html",
-  styleUrls: ["./open-classes.component.less"]
+  selector: 'app-open-classes',
+  templateUrl: './open-classes.component.html',
+  styleUrls: ['./open-classes.component.less']
 })
 export class OpenClassesComponent implements OnInit {
   lessons: Lesson[] = [
     {
       id: 1,
-      topic: "	CSS",
-      group: "62491",
-      teacher: "Терешко",
-      plan: "ссылка",
-      date: "26/11/2019/"
+      topic: 'CSS',
+      group: '62491',
+      teacher: 'Терешко',
+      plan: 'ссылка',
+      date: '26/11/2019/'
     },
     {
       id: 1,
-      topic: "	Методологии проектирования",
-      group: "62493",
-      teacher: "Тарасова",
-      plan: "ссылка",
-      date: "	15/09/2019"
+      topic: 'Методологии проектирования',
+      group: '62493',
+      teacher: 'Тарасова',
+      plan: 'ссылка',
+      date: '	15/09/2019'
     },
     {
       id: 1,
-      topic: "	Структуры данных",
-      group: "7к4911",
-      teacher: "Апанасевич",
-      plan: "ссылка",
-      date: "26/11/2019/"
+      topic: 'Структуры данных',
+      group: '7к4911',
+      teacher: 'Апанасевич',
+      plan: 'ссылка',
+      date: '26/11/2019/'
     }
   ];
 
