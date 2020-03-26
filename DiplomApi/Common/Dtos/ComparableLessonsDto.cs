18b0b5b19d4afead5b30d9ac4456acec8ae08cf4
Anastasiya Koush <a.koush@ya.ru>
2020-03-26 17:07:41 +0300

    Compare lessons

---
+++ DiplomApi/Common/Dtos/ComparableLessonsDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Dtos
+{
+  public class ComparableLessonsDto
+  {
+    public TeacherDto Teacher { get; set; }
+    public string Month { get; set; }
+    public DateTime? Date { get; set; }
+    public string Topic { get; set; }
+  }
+}
