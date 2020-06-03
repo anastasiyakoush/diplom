ed3cc470b75259aa443d35878a900e96c18bbbc7
Andrei Ledauski <c-aledauski@neogov.net>
2020-06-04 00:51:33 +0300

    Fix group

--- diplom/src/app/configuration/groups/update-groups/update-groups.component.ts
+++ diplom/src/app/configuration/groups/update-groups/update-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input, OnDestroy  } from '@angular/core';
+import { EndpointsService } from 'src/app/endpoints.service';
 
 @Component({
   selector: 'app-update-groups',
@@ -7,20 +8,51 @@ import { Component, OnInit, Output, EventEmitter, Input, OnDestroy  } from '@ang
 })
 export class UpdateGroupsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private endpointService: EndpointsService) {}
 
   @Output() cancelClick = new EventEmitter<any>();
   @Output() saveClick = new EventEmitter<any>();
   @Input() title: string;
+  @Input() update;
+  @Input() groupId;
+  group: any = {
 
-  ngOnInit() {}
+  };
+  plans: any[];
+
+  ngOnInit() {
+    this.endpointService.getPlans().subscribe(data => (this.plans = data));
+    if (this.update)
+      this.endpointService
+        .getGroupById(this.groupId)
+        .subscribe(data => (this.group = data));
+  }
   ngOnDestroy() { }
 
   cancel() {
     this.cancelClick.emit();
   }
 
+  onPlanChange(plan) {
+    this.group.uchebnyjPlanId = Number(plan);
+    this.group.uchebnyjPlan = this.plans.find(p=> p.id === Number(plan));
+  }
+
   save() {
-    this.saveClick.emit();
+    if (this.update) {
+      this.endpointService
+        .createGroup(this.group)
+        .subscribe(() => {
+          this.saveClick.emit();
+          location.reload();
+        });
+    } else {
+      this.endpointService
+        .createGroup({ name: this.group.name, uchebnyjPlanId: this.group.uchebnyjPlanId  })
+        .subscribe(() => {
+          this.saveClick.emit();
+          location.reload();
+        });
+    }
   }
 }
